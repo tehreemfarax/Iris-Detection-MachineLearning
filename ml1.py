e71595b62b34d67bb4ebfab8addd66a3e86e7c03
tehreemfarax <tehreem.faraz007@gmail.com>
2023-09-13 10:43:32 +0500

    first commit

---
+++ ml1.py
@@ -0,0 +1,74 @@
+from sklearn import datasets, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+iris = datasets.load_iris() #Loading the dataset
+print(iris.keys())
+
+iris = pd.DataFrame(
+    data= np.c_[iris['data'], iris['target']],
+    columns= iris['feature_names'] + ['target']
+    )
+print(iris)
+print(iris.head(6))
+species = []
+for i in range (len(iris['target'])):
+    if iris['target'][i] == 0:
+        species.append('setosa')
+    elif iris['target'][i] == 1:
+        species.append('versicolor')
+    else:
+        species.append('virginica')
+
+iris['species'] = species
+
+iris.groupby('species').size()
+
+print(iris.describe())
+
+setosa = iris[iris.species == 'setosa']
+versicolor = iris[iris.species == 'versicolor']
+virginica = iris[iris.species == 'virginica']
+
+fig, ax = plt.subplots()
+ax.set_xlabel('petal length (cm)')
+ax.set_ylabel('petal width (cm)')
+ax.grid()
+ax.set_title('Iris Petals')
+ax.legend()
+fig.set_size_inches(13,7)
+ax.scatter(setosa['petal length (cm)'], setosa['petal width (cm)'], label = 'Setosa', facecolor = 'blue')
+ax.scatter(versicolor['petal length (cm)'], versicolor['petal width (cm)'], label = 'Versicolor', facecolor = 'red')
+ax.scatter(virginica['petal length (cm)'], virginica['petal width (cm)'], label = 'Virginica', facecolor = 'green')
+
+x = iris.drop(['target','species'], axis=1)
+
+x=x.to_numpy()[:, (2,3)]
+y = iris['target']
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state = 42)
+
+log_reg = LogisticRegression()
+log_reg.fit(x_train, y_train)
+
+training_prediction = log_reg.predict(x_train)
+print(training_prediction)
+
+test_prediction = log_reg.predict(x_test)
+print(test_prediction)
+
+print('Precision, Recall, Confusion Matrix, in training \n ')
+
+print(metrics.classification_report(y_train, training_prediction, digits=3))
+print(metrics.classification_report(y_train, training_prediction))
+
+print('Precision, Recall, Confusion Matrix, in training \n ')
+
+print(metrics.classification_report(y_test,test_prediction, digits=3))
+print(metrics.classification_report(y_test,test_prediction))
+
+
+# plt.show()    
